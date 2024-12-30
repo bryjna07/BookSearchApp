311609bd01073d94cf74957e5f53bd25c533edbc
youngjin <bryjna07@nate.com>
2024-12-30 14:21:49 +0900

    Feat: 프로젝트 생성, 코드베이스 세팅

---
+++ BookSearchApp/BookSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookSearchApp
+//
+//  Created by t2023-m0033 on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
